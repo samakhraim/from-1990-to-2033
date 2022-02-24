dca3eb7592ee13ac75988fcc9004df922a085974
samakhraim <62477773+samakhraim@users.noreply.github.com>
2022-02-24 21:06:54 +0200

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+       
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        public void ButtonClicked(object c, EventArgs e)
+        {
+            string[] arr = new string[2033];
+
+            for (int i = 1990; i < 2034; i++)
+            {
+                Console.WriteLine(lable.Text += i.ToString() + "  ");
+            };
+          
+        }
+
+    }
+}
